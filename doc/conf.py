baa39a1c0558502e89c0c2fd40eb7791fd01afb3
Alan Saul <alan.daniel.saul@gmail.com>
2013-01-31 15:57:35 +0000

    Changed to matplotlib sphinxext

--- doc/conf.py
+++ doc/conf.py
@@ -144,7 +144,8 @@ sys.path.append(os.path.abspath('./sphinxext'))
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 print "Importing extensions"
               
-extensions = ['sphinxext.ipython_directive',
+print sys.path
+extensions = ['matplotlib.sphinxext.ipython_directive',
               'sphinx.ext.autodoc', 'sphinx.ext.viewcode'
               #'matplotlib.sphinxext.mathmpl',
               #'matplotlib.sphinxext.only_directives',

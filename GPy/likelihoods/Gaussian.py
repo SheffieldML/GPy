bdc89170d423591f78893da199b7c33fc255e55f
James Hensman <james.hensman@gmail.com>
2013-01-31 13:34:30 +0000

    added a Gaussian likelihood class

---
+++ GPy/likelihoods/Gaussian.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Gaussian:
+    def __init__(self,data,variance=1.,normalise=False):
+        self.data = data
+        if normalise:
+            foo
+        self._variance = variance
+    def _get_params(self):
+        return np.asarray(self.variance)
+    def _set_params(self,x):
+        self._variance = x
+    def fit(self):
+        pass
+    def _gradients(self,foo):
+        return bar(foo)
---
+++ GPy/likelihoods/__init__.py
@@ -0,0 +1,3 @@
+from EP import EP
+from Gaussian import Gaussian
+# TODO: from Laplace import Laplace

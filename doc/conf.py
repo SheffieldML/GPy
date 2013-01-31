a605416d060e71441eae899798187eaddbaf9834
Alan Saul <alan.daniel.saul@gmail.com>
2013-01-31 11:15:54 +0000

    Adding pylab mock module

--- doc/conf.py
+++ doc/conf.py
@@ -13,6 +13,29 @@
 
 import sys, os
 
+#Mocking uninstalled modules: https://read-the-docs.readthedocs.org/en/latest/faq.html
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['pylab']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.

d4a20232c06f2dc519e6665c1de5d735f85fd881
Alan Saul <alan.daniel.saul@gmail.com>
2013-01-31 11:23:58 +0000

    Forgot exceptions import

--- doc/conf.py
+++ doc/conf.py
@@ -11,7 +11,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import sys, os, exceptions
 
 #Mocking uninstalled modules: https://read-the-docs.readthedocs.org/en/latest/faq.html
 

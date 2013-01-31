5c8aebdb596b63317ef59cb8bd0aac92f1ce9c38
Alan Saul <alan.daniel.saul@gmail.com>
2013-01-31 12:21:31 +0000

    more

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,7 @@ import sys, os, exceptions
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
+#sys.path.insert(0, os.path.abspath('..'))
 
 # If your extensions are in another directory, add it here. If the directory
 # is relative to the documentation root, use os.path.abspath to make it

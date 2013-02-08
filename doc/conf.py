2422945f6c39ef87372f560a30935254488d1683
Alan Saul <alan.daniel.saul@gmail.com>
2013-02-08 13:56:53 +0000

    Changed path to append it rather than inset it

--- doc/conf.py
+++ doc/conf.py
@@ -37,12 +37,12 @@ try:
 except ImportError:
     print "no sphinx"
 
-sys.path.insert(0, os.getcwd() + "/..")
+#sys.path.insert(0, os.getcwd() + "/..")
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # If your extensions are in another directory, add it here. If the directory
 # is relative to the documentation root, use os.path.abspath to make it

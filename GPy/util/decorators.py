5781fbd582180a1977ae1a22b2f73b473f4b8084
Nicolo Fusi <nicolo.fusi@gmail.com>
2013-03-12 16:42:52 +0000

    now it actually works

--- GPy/util/decorators.py
+++ GPy/util/decorators.py
@@ -2,9 +2,10 @@ import numpy as np
 from functools import wraps
 
 def silence_errors(f):
-    status = np.seterr(all='ignore')
     @wraps(f)
     def wrapper(*args, **kwds):
-        return f(*args, **kwds)
-    np.seterr(**status)
+        status = np.seterr(all='ignore')
+        result = f(*args, **kwds)
+        np.seterr(**status)
+        return result
     return wrapper

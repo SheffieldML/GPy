f3ea01f76dcca8ecd11954eb545ecba471a11918
Ricardo <rick70x7@gmail.com>
2013-04-15 08:14:01 +0100

    Reverting last change

--- GPy/models/sparse_GP.py
+++ GPy/models/sparse_GP.py
@@ -185,11 +185,12 @@ class sparse_GP(GP):
         self.likelihood._set_params(p[self.Z.size+self.kern.Nparam:])
         self._compute_kernel_matrices()
         if self.auto_scale_factor:
-            if self.likelihood.is_heteroscedastic:
-                self.scale_factor = max(100.,(self.psi2_beta_scaled.sum(0).max()))
-                print self.scale_factor
-            else:
-                self.scale_factor = np.sqrt(self.psi2.sum(0).mean()*self.likelihood.precision)
+            self.scale_factor = np.sqrt(self.psi2.sum(0).mean()*self.likelihood.precision)
+        #if self.auto_scale_factor:
+        #    if self.likelihood.is_heteroscedastic:
+        #        self.scale_factor = max(1,np.sqrt(self.psi2_beta_scaled.sum(0).mean()))
+        #    else:
+        #        self.scale_factor = np.sqrt(self.psi2.sum(0).mean()*self.likelihood.precision)
         self._computations()
 
     def _get_params(self):

00a5f8e5f63c7b3fcef45cab7f142b3af7b67705
Neil Lawrence <lawrennd@gmail.com>
2013-08-22 14:03:59 +0200

    Added gpx dataset.

--- GPy/kern/__init__.py
+++ GPy/kern/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2012, GPy authors (see AUTHORS.txt).
+# Copyright (c) 2012, 2013 GPy authors (see AUTHORS.txt).
 # Licensed under the BSD 3-clause license (see LICENSE.txt)
 
 from constructors import *
@@ -6,4 +6,4 @@ try:
     from constructors import rbf_sympy, sympykern # these depend on sympy
 except:
     pass
-from kern import kern
+from kern import *

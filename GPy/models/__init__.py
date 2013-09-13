d653921bf35d1c502c847bd5696479ef719b75ea
Ricardo <acq11ra@sheffield.ac.uk>
2013-09-13 13:09:55 +0100

    Useless files deleted

--- GPy/models/__init__.py
+++ GPy/models/__init__.py
@@ -11,7 +11,5 @@ from gplvm import GPLVM
 from warped_gp import WarpedGP
 from bayesian_gplvm import BayesianGPLVM
 from mrd import MRD
-from gp_multioutput import GPMultioutput
 from gp_multioutput_regression import GPMultioutputRegression
 from sparse_gp_multioutput_regression import SparseGPMultioutputRegression
-from sparse_gp_multioutput import SparseGPMultioutput

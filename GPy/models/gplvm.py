2762e158fdee3156d8fb35774b05810924331915
Ricardo <acq11ra@sheffield.ac.uk>
2013-09-17 17:44:00 +0100

    normalize_Y is passed to core function

--- GPy/models/gplvm.py
+++ GPy/models/gplvm.py
@@ -33,7 +33,7 @@ class GPLVM(GP):
         if kernel is None:
             kernel = kern.rbf(input_dim, ARD=input_dim > 1) + kern.bias(input_dim, np.exp(-2))
         likelihood = Gaussian(Y, normalize=normalize_Y, variance=np.exp(-2.))
-        GP.__init__(self, X, likelihood, kernel, normalize_X=False)
+        GP.__init__(self, X, likelihood, kernel, normalize_X=False,normalize_Y = normalize_Y)
         self.set_prior('.*X', Gaussian_prior(0, 1))
         self.ensure_default_constraints()
 

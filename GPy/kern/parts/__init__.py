9a81c48500edf8f1babaf6b49541c454159fc9a2
James Hensman <james.hensman@gmail.com>
2013-09-20 11:02:35 +0100

    added hetero back to the init

--- GPy/kern/parts/__init__.py
+++ GPy/kern/parts/__init__.py
@@ -5,7 +5,7 @@ import exponential
 import finite_dimensional
 import fixed
 import gibbs
-#import hetero #hetero.py is not commited: omitting for now. JH. 
+import hetero
 import hierarchical
 import independent_outputs
 import linear

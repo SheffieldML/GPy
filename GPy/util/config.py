df97f7814efb0589868cfe9e6ef4026414fb5a83
James Hensman <james.hensman@gmail.com>
2013-11-13 13:40:44 +0000

    better handling of missing config files

--- GPy/util/config.py
+++ GPy/util/config.py
@@ -14,6 +14,9 @@ print default_file, os.path.isfile(default_file)
 # 1. check if the user has a ~/.gpy_config.cfg
 if os.path.isfile(user_file):
     config.read(user_file)
-else:
+elif os.path.isfile(default_file):
     # 2. if not, use the default one
     config.read(default_file)
+else:
+    #3. panic
+    raise ValueError, "no configuration file found"

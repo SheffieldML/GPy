83a495645d1a41039967162443fc8173224017b6
James Hensman <james.hensman@gmail.com>
2013-12-04 11:32:12 +0000

    some more messing with the likelihood directory

---
+++ GPy/likelihoods/__init__.py
@@ -0,0 +1,6 @@
+from bernoulli import Bernoulli
+from exponential import Exponential
+from gaussian import Gaussian
+from gamma import Gamma
+from poisson import Poisson
+from student_t import Student_t

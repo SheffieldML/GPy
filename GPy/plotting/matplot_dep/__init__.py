1ab26de56e17e4b8d10735483a48db615af10662
Ricardo <acq11ra@sheffield.ac.uk>
2014-01-28 13:46:42 +0000

    New files

---
+++ GPy/plotting/matplot_dep/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2014, GPy authors (see AUTHORS.txt).
+# Licensed under the BSD 3-clause license (see LICENSE.txt)
+
+import base_plots
+import models_plots
+import priors_plots
+import variational_plots
+import kernel_plots
+import svig_plots
+import dim_reduction_plots
+import mapping_plots
+import Tango
+import visualize
+import latent_space_visualizations
+import netpbmfile
+import inference_plots

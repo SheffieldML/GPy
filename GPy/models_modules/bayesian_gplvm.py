1d932cb1b9a21f75abfff889731267a7df2b28d7
mzwiessele <ibinbei@gmail.com>
2014-01-29 10:46:37 +0000

    bgplvm steepest gradient map update

--- GPy/models_modules/bayesian_gplvm.py
+++ GPy/models_modules/bayesian_gplvm.py
@@ -215,12 +215,13 @@ class BayesianGPLVM(SparseGP, GPLVM):
 
         from matplotlib.cm import get_cmap
         from GPy.util.latent_space_visualizations.controllers.imshow_controller import ImAnnotateController
+        if not 'cmap' in kwargs.keys():
+            kwargs.update(cmap=get_cmap('jet'),))
         controller = ImAnnotateController(ax,
                                       plot_function,
                                       tuple(self.X.min(0)[:, significant_dims]) + tuple(self.X.max(0)[:, significant_dims]),
                                       resolution=resolution,
                                       aspect=aspect,
-                                      cmap=get_cmap('jet'),
                                       **kwargs)
         ax.legend()
         ax.figure.tight_layout()

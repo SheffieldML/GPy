4215f5fb2892792b02f351208aca4aa11cd2c8f8
James Hensman <james.hensman@gmail.com>
2014-02-24 15:56:06 +0000

    tidying in kern

--- GPy/kern/__init__.py
+++ GPy/kern/__init__.py
@@ -7,7 +7,7 @@ from _src.stationary import Exponential, Matern32, Matern52, ExpQuad, RatQuad, C
 from _src.mlp import MLP
 from _src.periodic import PeriodicExponential, PeriodicMatern32, PeriodicMatern52
 from _src.independent_outputs import IndependentOutputs
-#import coregionalize
+from _src.coregionalize import Coregionalize
 #import eq_ode1
 #import finite_dimensional
 #import fixed
@@ -15,9 +15,6 @@ from _src.independent_outputs import IndependentOutputs
 #import hetero
 #import hierarchical
 #import ODE_1
-#import periodic_exponential
-#import periodic_Matern32
-#import periodic_Matern52
 #import poly
 #import rbfcos
 #import rbf

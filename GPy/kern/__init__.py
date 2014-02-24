76bb673326a54063049b31980333216397421fd7
James Hensman <james.hensman@gmail.com>
2014-02-24 16:19:03 +0000

    hierarchical kern should be working. I'll let you know then the tests are up...

--- GPy/kern/__init__.py
+++ GPy/kern/__init__.py
@@ -6,18 +6,5 @@ from _src.brownian import Brownian
 from _src.stationary import Exponential, Matern32, Matern52, ExpQuad, RatQuad, Cosine
 from _src.mlp import MLP
 from _src.periodic import PeriodicExponential, PeriodicMatern32, PeriodicMatern52
-from _src.independent_outputs import IndependentOutputs
+from _src.independent_outputs import IndependentOutputs, Hierarchical
 from _src.coregionalize import Coregionalize
-#import eq_ode1
-#import finite_dimensional
-#import fixed
-#import gibbs
-#import hetero
-#import hierarchical
-#import ODE_1
-#import poly
-#import rbfcos
-#import rbf
-#import rbf_inv
-#import spline
-#import symmetric

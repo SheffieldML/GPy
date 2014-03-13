3d6a69e5f090c17ffe325ad9f072fe0382397a2b
Max Zwiessele <ibinbei@gmail.com>
2014-03-13 12:28:56 +0000

    we need to update all the tests: here discontinuous kernel testsee messing, mrd and bgplvm model tests not needed anymore

--- GPy/testing/parameterized_tests.py
+++ GPy/testing/parameterized_tests.py
@@ -16,21 +16,21 @@ class Test(unittest.TestCase):
         from GPy.core.parameterization import Param
         from GPy.core.parameterization.transformations import Logistic
         self.param = Param('param', np.random.rand(25,2), Logistic(0, 1))
-        
+
         self.test1 = GPy.core.Parameterized("test model")
         self.test1.add_parameter(self.white)
         self.test1.add_parameter(self.rbf, 0)
         self.test1.add_parameter(self.param)
-        
+
         x = np.linspace(-2,6,4)[:,None]
         y = np.sin(x)
         self.testmodel = GPy.models.GPRegression(x,y)
-        
+
     def test_add_parameter(self):
         self.assertEquals(self.rbf._parent_index_, 0)
         self.assertEquals(self.white._parent_index_, 1)
         pass
-    
+
     def test_fixes(self):
         self.white.fix(warning=False)
         self.test1.remove_parameter(self.test1.param)
@@ -41,18 +41,18 @@ class Test(unittest.TestCase):
 
         self.test1.add_parameter(self.white, 0)
         self.assertListEqual(self.test1._fixes_.tolist(),[FIXED,UNFIXED,UNFIXED])
-        
+
     def test_remove_parameter(self):
         from GPy.core.parameterization.transformations import FIXED, UNFIXED, __fixed__, Logexp
         self.white.fix()
         self.test1.remove_parameter(self.white)
         self.assertIs(self.test1._fixes_,None)
-        
+
         self.assertListEqual(self.white._fixes_.tolist(), [FIXED])
         self.assertEquals(self.white.constraints._offset, 0)
         self.assertIs(self.test1.constraints, self.rbf.constraints._param_index_ops)
         self.assertIs(self.test1.constraints, self.param.constraints._param_index_ops)        
-        
+
         self.test1.add_parameter(self.white, 0)
         self.assertIs(self.test1.constraints, self.white.constraints._param_index_ops)
         self.assertIs(self.test1.constraints, self.rbf.constraints._param_index_ops)
@@ -60,17 +60,17 @@ class Test(unittest.TestCase):
         self.assertListEqual(self.test1.constraints[__fixed__].tolist(), [0])
         self.assertIs(self.white._fixes_,None)
         self.assertListEqual(self.test1._fixes_.tolist(),[FIXED] + [UNFIXED] * 52)
-        
+
         self.test1.remove_parameter(self.white)
         self.assertIs(self.test1._fixes_,None)
         self.assertListEqual(self.white._fixes_.tolist(), [FIXED])
         self.assertIs(self.test1.constraints, self.rbf.constraints._param_index_ops)
         self.assertIs(self.test1.constraints, self.param.constraints._param_index_ops)
         self.assertListEqual(self.test1.constraints[Logexp()].tolist(), [0,1])
-        
+
     def test_add_parameter_already_in_hirarchy(self):
         self.assertRaises(HierarchyError, self.test1.add_parameter, self.white._parameters_[0])        
-        
+
     def test_default_constraints(self):
         self.assertIs(self.rbf.variance.constraints._param_index_ops, self.rbf.constraints._param_index_ops)
         self.assertIs(self.test1.constraints, self.rbf.constraints._param_index_ops)
@@ -83,7 +83,7 @@ class Test(unittest.TestCase):
         self.rbf.constrain(GPy.transformations.Square(), False)
         self.assertListEqual(self.test1.constraints[GPy.transformations.Square()].tolist(), range(2))
         self.assertListEqual(self.test1.constraints[GPy.transformations.Logexp()].tolist(), [2])
-        
+
         self.test1.remove_parameter(self.rbf)
         self.assertListEqual(self.test1.constraints[GPy.transformations.Square()].tolist(), [])
 

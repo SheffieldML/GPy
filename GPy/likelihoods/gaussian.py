bea74ac5231e47cce34170273771fd61a39e6b73
Ricardo <acq11ra@sheffield.ac.uk>
2014-03-17 18:59:37 +0000

    new function added

--- GPy/likelihoods/gaussian.py
+++ GPy/likelihoods/gaussian.py
@@ -50,6 +50,12 @@ class Gaussian(Likelihood):
         if isinstance(gp_link, link_functions.Identity):
             self.log_concave = True
 
+    def betaY(self,Y,Y_metadata=None):
+        return Y/self.gaussian_variance(Y_metadata)
+
+    def gaussian_variance(self, Y_metadata=None):
+        return self.variance
+
     def covariance_matrix(self, Y, Y_metadata=None):
         return np.eye(Y.shape[0]) * self.variance
 

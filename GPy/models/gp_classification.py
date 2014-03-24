5c28fd4d5ede012e282a497231fe3ed8a1d04202
Max Zwiessele <ibinbei@gmail.com>
2014-03-24 11:32:41 +0000

    deleted unused imports

--- GPy/models/gp_classification.py
+++ GPy/models/gp_classification.py
@@ -2,7 +2,6 @@
 # Copyright (c) 2013, the GPy Authors (see AUTHORS.txt)
 # Licensed under the BSD 3-clause license (see LICENSE.txt)
 
-import numpy as np
 from ..core import GP
 from .. import likelihoods
 from .. import kern

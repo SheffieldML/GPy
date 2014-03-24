Fixed bug in product kernel gradients diag wrt to X

GPy/kern/_src/prod.py
@@ -58,8 +58,6 @@ class Prod(CombinationKernel):
     def gradients_X_diag(self, dL_dKdiag, X):
         target = np.zeros(X.shape)
         for k1,k2 in itertools.combinations(self.parts, 2):
-            target += k1.gradients_X(dL_dKdiag*k2.Kdiag(X), X)
-            target += k2.gradients_X(dL_dKdiag*k1.Kdiag(X), X)
+            target += k1.gradients_X_diag(dL_dKdiag*k2.Kdiag(X), X)
+            target += k2.gradients_X_diag(dL_dKdiag*k1.Kdiag(X), X)
         return target
-
-

GPy/testing/model_tests.py
@@ -59,9 +59,10 @@ class MiscTests(unittest.TestCase):
         #np.testing.assert_almost_equal(mu_hat, mu)
 
     def test_likelihood_replicate(self):
+        tol = 1e-5
         m = GPy.models.GPRegression(self.X, self.Y)
         m2 = GPy.models.GPRegression(self.X, self.Y)
-        np.testing.assert_equal(m.log_likelihood(), m2.log_likelihood())
+        np.testing.assert_equal(m.log_likelihood(), m2.log_likelihood(), tol=tol)
         m.randomize()
         m2[:] = m[''].values()
         np.testing.assert_equal(m.log_likelihood(), m2.log_likelihood())
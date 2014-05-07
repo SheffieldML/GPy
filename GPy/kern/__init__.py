beebf6933a3681c573ffd8e8b7f978e549e2c959
James Hensman <james.hensman@gmail.com>
2014-05-07 14:53:10 +0100

    added polynomial kernel

--- GPy/kern/__init__.py
+++ GPy/kern/__init__.py
@@ -10,6 +10,7 @@ from _src.independent_outputs import IndependentOutputs, Hierarchical
 from _src.coregionalize import Coregionalize
 from _src.ssrbf import SSRBF # TODO: ZD: did you remove this?
 from _src.ODE_UY import ODE_UY
+from _src.poly import Poly
 #from _src.ODE_UYC import ODE_UYC ADD THIS FILE TO THE REPO!!
 #from _src.ODE_st import ODE_st
 # TODO: put this in an init file somewhere

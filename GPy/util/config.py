0fde50f56df88e6448c0be22cee42a0afedd2cab
Neil Lawrence <lawrennd@gmail.com>
2014-05-28 10:25:02 +0100

    Changes to configuration file set up: now uses defaults.cfg, installation.cfg and searches locally for .gpy_user.cfg in the users home directory.

--- GPy/util/config.py
+++ GPy/util/config.py
@@ -5,18 +5,19 @@ import ConfigParser
 import os
 config = ConfigParser.ConfigParser()
 
-home = os.getenv('HOME') or os.getenv('USERPROFILE')
-user_file = os.path.join(home,'.gpy_config.cfg')
-default_file = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'gpy_config.cfg'))
-# print user_file, os.path.isfile(user_file)
-# print default_file, os.path.isfile(default_file)
+# This is the default configuration file that always needs to be present.
+default_file = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'defaults.cfg'))
 
-# 1. check if the user has a ~/.gpy_config.cfg
-if os.path.isfile(user_file):
-    config.read(user_file)
-elif os.path.isfile(default_file):
-    # 2. if not, use the default one
-    config.read(default_file)
-else:
-    #3. panic
-    raise ValueError, "no configuration file found"
+# These files are optional 
+# This specifies configurations that are typically specific to the machine (it is found alongside the GPy installation).
+local_file = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'machine.cfg'))
+
+# This specifies configurations specific to the user (it is found in the user home directory)
+home = os.getenv('HOME') or os.getenv('USERPROFILE')
+user_file = os.path.join(home,'.gpy_user.cfg')
+
+# Read in the given files.
+config.readfp(open(default_file))
+config.read([local_file, user_file])
+if not config:
+    raise ValueError, "No configuration file found at either " + user_file + " or " + local_file + " or " + default_file + "."

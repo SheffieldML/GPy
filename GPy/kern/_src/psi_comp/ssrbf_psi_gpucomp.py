92fecbb4efd4a4cb81725b4be7281fce57830479
Zhenwen Dai <z.dai@sheffield.ac.uk>
2014-06-26 14:01:58 +0100

    remove dependence of cublas from rbf kernel

--- GPy/kern/_src/psi_comp/ssrbf_psi_gpucomp.py
+++ GPy/kern/_src/psi_comp/ssrbf_psi_gpucomp.py
@@ -293,7 +293,6 @@ class PSICOMP_SSRBF_GPU(PSICOMP_RBF):
 
     def __init__(self, threadnum=128, blocknum=15, GPU_direct=False):
         self.GPU_direct = GPU_direct
-        self.cublas_handle = gpu_init.cublas_handle
         self.gpuCache = None
         
         self.threadnum = threadnum

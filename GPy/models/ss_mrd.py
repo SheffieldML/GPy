2c88528ebd96ecb8ce53a9614bdfc3d0a41ccc61
Zhenwen Dai <z.dai@sheffield.ac.uk>
2014-06-27 17:25:08 +0100

    ss_mrd with parameter tied

--- GPy/models/ss_mrd.py
+++ GPy/models/ss_mrd.py
@@ -19,4 +19,13 @@ class SSMRD(Model):
                                name='model_'+str(i)) for i,y in enumerate(Ylist)]
         self.add_parameters(*(self.models))
         self.updates = True
+        
+        [[self.models[j].X.mean.flat[i:i+1].tie('mean_'+str(i)) for j in xrange(len(self.models))] for i in xrange(self.models[0].X.mean.size)]
+        [[self.models[j].X.variance.flat[i:i+1].tie('var_'+str(i)) for j in xrange(len(self.models))] for i in xrange(self.models[0].X.variance.size)]
 
+    def parameters_changed(self):
+        super(SSMRD, self).parameters_changed()        
+        self._log_marginal_likelihood = sum([m._log_marginal_likelihood for m in self.models])
+
+    def log_likelihood(self):
+        return self._log_marginal_likelihood

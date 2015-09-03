9fb894f3d8bc831e785d2b6d73ffeef80e3e1042
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2015-09-03 17:33:03 +0200

    FIX: missing compatibility Py2/3

--- GPy/inference/mcmc/samplers.py
+++ GPy/inference/mcmc/samplers.py
@@ -1,6 +1,6 @@
 # ## Copyright (c) 2014, Zhenwen Dai
 # Licensed under the BSD 3-clause license (see LICENSE.txt)
-
+from __future__ import print_function
 
 import numpy as np
 from scipy import linalg, optimize

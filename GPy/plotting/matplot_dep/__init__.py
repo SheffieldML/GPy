7ef32f461569fa5dbc7220cb343acb55bd512f1b
Max Zwiessele <ibinbei@gmail.com>
2015-09-12 13:59:16 +0100

    Update __init__.py

--- GPy/plotting/matplot_dep/__init__.py
+++ GPy/plotting/matplot_dep/__init__.py
@@ -1,18 +1,18 @@
 # Copyright (c) 2014, GPy authors (see AUTHORS.txt).
 # Licensed under the BSD 3-clause license (see LICENSE.txt)
 
-import .base_plots
-import .models_plots
-import .priors_plots
-import .variational_plots
-import .kernel_plots
-import .dim_reduction_plots
-import .mapping_plots
-import .Tango
-import .visualize
-import .latent_space_visualizations
-import .netpbmfile
-import .inference_plots
-import .maps
-import .img_plots
+from . import base_plots
+from . import models_plots
+from . import priors_plots
+from . import variational_plots
+from . import kernel_plots
+from . import dim_reduction_plots
+from . import mapping_plots
+from . import Tango
+from . import visualize
+from . import latent_space_visualizations
+from . import netpbmfile
+from . import inference_plots
+from . import maps
+from . import img_plots
 from .ssgplvm import SSGPLVM_plot   

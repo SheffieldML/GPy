f71b57c24ed86e73c3661aa72b63b0236f70b316
Alan Saul <alan.daniel.saul@gmail.com>
2015-09-14 14:38:50 +0100

    Missing numpy import

--- GPy/plotting/matplot_dep/base_plots.py
+++ GPy/plotting/matplot_dep/base_plots.py
@@ -1,6 +1,7 @@
 # #Copyright (c) 2012, GPy authors (see AUTHORS.txt).
 # Licensed under the BSD 3-clause license (see LICENSE.txt)
 from matplotlib import pyplot as pb
+import numpy as np
 
 def ax_default(fignum, ax):
     if ax is None:

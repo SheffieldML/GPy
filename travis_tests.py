465d36674067c77d3726192f22ae2c33f2e7a925
mzwiessele <ibinbei@gmail.com>
2015-10-01 07:31:20 +0100

    [travis] testing codecoverage

---
+++ travis_tests.py
@@ -0,0 +1,14 @@
+'''
+Created on 30 Sep 2015
+
+@author: Max Zwiessele
+'''
+
+#!/usr/bin/env python
+
+import matplotlib 
+matplotlib.use('svg')
+
+import nose 
+nose.main('applygpy', defaultTest='applygpy/tests')  
+

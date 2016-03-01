885d3722cce0d7ee492f0d0d0f8f035954af70f6
Max Zwiessele <ibinbei@gmail.com>
2016-03-01 10:02:02 +0000

    [mrd] plot_scales and plot_latent added

--- GPy/testing/__init__.py
+++ GPy/testing/__init__.py
@@ -1,14 +1,9 @@
-# Copyright (c) 2014, Max Zwiessele
+# Copyright (c) 2014, Max Zwiessele, GPy Authors
 # Licensed under the BSD 3-clause license (see LICENSE.txt)
-"""
-
-MaxZ
-
-"""
 import unittest
 import sys
 
 def deepTest(reason):
     if reason:
         return lambda x:x
-    return unittest.skip("Not deep scanning, enable deepscan by adding 'deep' argument")
+    return unittest.skip("Not deep scanning, enable deepscan by adding 'deep' argument to unittest call")

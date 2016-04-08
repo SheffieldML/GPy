8358645e3764379a2b41e3d1e68f90bc3bc598a7
Max Zwiessele <ibinbei@gmail.com>
2016-04-08 09:47:08 +0100

    [rtd] last removal of rtd

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -77,12 +77,13 @@ extensions = [
 #    def __getattr__(cls, name):
 #            return Mock()
 #
-MOCK_MODULES = ['numpy', 'scipy.linalg.blas', 'blas', 'scipy.optimize', 'scipy.optimize.linesearch', 'scipy.linalg',
+MOCK_MODULES = ['scipy.linalg.blas', 'blas', 'scipy.optimize', 'scipy.optimize.linesearch', 'scipy.linalg',
                 'scipy', 'scipy.special', 'scipy.integrate', 'scipy.io', 'scipy.stats',
                 'sympy', 'sympy.utilities.iterables', 'sympy.utilities.lambdify',
                 'sympy.utilities', 'sympy.utilities.codegen', 'sympy.core.cache',
                 'sympy.core', 'sympy.parsing', 'sympy.parsing.sympy_parser',
-                'nose', 'nose.tools']
+                'nose', 'nose.tools'
+                ]
 
 autodoc_mock_imports = MOCK_MODULES
 #

ed3f6c56c22a7fafa4aa7e4aefd2639e00c0a668
Akash Kumar Dhaka <akash.dhaka@aalto.fi>
2017-07-03 18:24:02 +0300

    commiting code for some changes to api for calculating ep_gradients, also fixing some issues with gaussian hermite quadrature, no we have both avaialable ...

--- GPy/likelihoods/gaussian.py
+++ GPy/likelihoods/gaussian.py
@@ -57,7 +57,7 @@ class Gaussian(Likelihood):
     def update_gradients(self, grad):
         self.variance.gradient = grad
 
-    def exact_inference_gradients(self, dL_dKdiag,Y_metadata=None):
+    def ep_gradients(self, Y, cav_tau, cav_v, dL_dKdiag,Y_metadata=None):
         return dL_dKdiag.sum()
 
     def _preprocess_values(self, Y):

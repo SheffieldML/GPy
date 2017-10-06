f7f8f31b0c0f1febd94af35d606e8c1e09b0a61c
mzwiessele <ibinbei@gmail.com>
2017-10-06 13:52:14 +0100

    fix: pkg: faster throughput:

--- .appveyor_twine_upload.bat
+++ .appveyor_twine_upload.bat
@@ -3,20 +3,3 @@ IF "%APPVEYOR_REPO_BRANCH%"=="deploy" (
 ) ELSE (
   ECHO Only deploy on deploy branch
 )
-
-
-
-  echo not deploying on other branches, other than deploy
-
-
-
-#ps: >-
-#    If ($env:APPVEYOR_REPO_BRANCH -eq 'devel') { 
-#        echo not deploying on devel # twine upload --skip-existing -r test dist/*
-#    }
-#    ElseIf ($env:APPVEYOR_REPO_BRANCH -eq 'deploy') {  
-#        twine upload --skip-existing dist/*
-#    }
-#    Else {
-#        echo not deploying on other branches
-#    }

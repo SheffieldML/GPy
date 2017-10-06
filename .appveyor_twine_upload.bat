d821a2659dee624672b57bdce001384a25246891
mzwiessele <ibinbei@gmail.com>
2017-10-06 13:54:34 +0100

    fix: pkg: faster throughput:

--- .appveyor_twine_upload.bat
+++ .appveyor_twine_upload.bat
@@ -1,5 +1,5 @@
 IF "%APPVEYOR_REPO_BRANCH%"=="deploy" (
   ECHO "twine upload --skip-existing dist/*" 1>&2
 ) ELSE (
-  ECHO Only deploy on deploy branch
+  ECHO Only deploy on deploy branch 1>&2
 )

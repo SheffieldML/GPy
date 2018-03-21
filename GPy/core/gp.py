90c2912acefaf7d0f595e62f664a2317cea104ec
Diego Torrejon <diego.torrejon.medina@gmail.com>
2018-03-21 18:00:13 -0400

    Fixes the dimensions of the samples output

--- GPy/core/gp.py
+++ GPy/core/gp.py
@@ -592,7 +592,7 @@ class GP(Model):
         if self.output_dim == 1:
             return sim_one_dim(m, v)
         else:
-            fsim = np.empty((self.output_dim, self.num_data, size))
+            fsim = np.empty((self.output_dim, len(X), size))
             for d in range(self.output_dim):
                 if full_cov and v.ndim == 3:
                     fsim[d] = sim_one_dim(m[:, d], v[:, :, d])

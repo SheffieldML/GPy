07371cd77770d6963e94394c909fdd78a391066f
bobturneruk <bob.turner.uk@gmail.com>
2020-04-03 10:14:11 +0100

    add placeholder

--- GPy/core/__init__.py
+++ GPy/core/__init__.py
@@ -1,6 +1,11 @@
 # Copyright (c) 2012-2014, GPy authors (see AUTHORS.txt).
 # Licensed under the BSD 3-clause license (see LICENSE.txt)
 
+"""
+This module does...
+
+"""
+
 from GPy.core.model import Model
 from .parameterization import Param, Parameterized
 from . import parameterization

4e411c4b26f738dcbd098c5b8c697a9e5e607854
bobturneruk <bob.turner.uk@gmail.com>
2020-04-24 10:19:36 +0100

    likelihoods

--- GPy/likelihoods/__init__.py
+++ GPy/likelihoods/__init__.py
@@ -1,3 +1,16 @@
+"""
+Introduction
+^^^^^^^^^^^^
+
+'Likelihood' in this context is a measure of how well a model *f* predicts a dataset *y*. The importance of likelihoods in Gaussian Processes is in determining the 'best' values of kernel and noise hyperparamters to relate known, observed and unobserved data. The purpose of optimizing a model (e.g. :py:class:`GPy.models.GPRegression`) is to determine the 'best' hyperparameters i.e. those that minimize negative log marginal likelihood.
+
+.. inheritance-diagram:: GPy.likelihoods.likelihood GPy.likelihoods.mixed_noise.MixedNoise
+   :top-classes: GPy.core.parameterization.parameterized.Parameterized
+
+Most likelihood classes inherit directly from :py:class:`GPy.likelihoods.likelihood`, although an intermediary class :py:class:`GPy.likelihoods.mixed_noise.MixedNoise` is used by :py:class:`GPy.likelihoods.multioutput_likelihood`.
+
+"""
+
 from .bernoulli import Bernoulli
 from .exponential import Exponential
 from .gaussian import Gaussian, HeteroscedasticGaussian

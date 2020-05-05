dec5bf8cefcc21c1363082fb1d2eb75a32b78af3
bobturneruk <bob.turner.uk@gmail.com>
2020-05-05 12:46:57 +0100

    using pods

--- GPy/examples/__init__.py
+++ GPy/examples/__init__.py
@@ -4,7 +4,7 @@
 Examples for GPy.
 
 The examples in this package usually depend on `pods <https://github.com/sods/ods>`_ so make sure 
-you have that installed before running examples.
+you have that installed before running examples. The easiest way to do this is to run `pip install pods`. `pods` enables access to 3rd party data required for most of the examples. 
 """
 from . import classification
 from . import regression

15f67cd8ff8b2c1c2ddef16e8f99de412961b8a4
bobturneruk <r.d.turner@sheffield.ac.uk>
2020-09-24 11:14:14 +0100

    remove deprecated parameter

--- GPy/testing/plotting_tests.py
+++ GPy/testing/plotting_tests.py
@@ -38,7 +38,7 @@ from nose import SkipTest
 
 try:
     import matplotlib
-    matplotlib.use('agg', warn=False)
+    matplotlib.use('agg')
 except ImportError:
     # matplotlib not installed
     from nose import SkipTest

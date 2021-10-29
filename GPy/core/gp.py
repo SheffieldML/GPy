a133947b424cbe4f057f816eb387db96d5f8d424
gehbiszumeis <16896724+gehbiszumeis@users.noreply.github.com>
2021-10-29 13:35:13 +0200

    Y_metadata conversion from serializable to np.array when loading from dict

--- GPy/core/gp.py
+++ GPy/core/gp.py
@@ -163,9 +163,10 @@ class GP(Model):
             input_dict["mean_function"] = mean_function
         input_dict["inference_method"] = GPy.inference.latent_function_inference.LatentFunctionInference.from_dict(input_dict["inference_method"])
 
-        #FIXME: Assumes the Y_metadata is serializable. We should create a Metadata class
-        Y_metadata = input_dict.get("Y_metadata")
-        input_dict["Y_metadata"] = Y_metadata
+        # converts Y_metadata from serializable to array. We should create a Metadata class
+        # Y_metadata = input_dict.get("Y_metadata")
+        # input_dict["Y_metadata"] = Y_metadata
+        input_dict["Y_metadata"] = {k: np.array(input_dict['Y_metadata'][k]) for k in input_dict['Y_metadata'].keys()}
 
         normalizer = input_dict.get("normalizer")
         if normalizer is not None:

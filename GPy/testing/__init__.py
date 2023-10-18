654edd6f2214875473b2b496e43d5f135e5c538b
Martin Bubel <martin.bubel@itwm.fraunhofer.de>
2023-10-18 20:17:09 +0200

    remove unittest stuff from testing init file

--- GPy/testing/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2014, Max Zwiessele, GPy Authors
-# Licensed under the BSD 3-clause license (see LICENSE.txt)
-import unittest
-import sys
-
-def deepTest(reason):
-    if reason:
-        return lambda x:x
-    return unittest.skip("Not deep scanning, enable deepscan by adding 'deep' argument to unittest call")
